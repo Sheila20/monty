43ee29942663e2a27c735602431c9277a32370af
Sheila20 <shilanyambura47@gmail.com>
2022-09-07 05:11:05 -0700

    f

--- free_stack.c
+++ free_stack.c
@@ -1,27 +1,17 @@
 #include "monty.h"
 
 /**
- * free_stack - frees the stack on exit
- * @status: exit status
- * @arg: double pointer to the stack
- * Return: void
+ * free_stack - frees a doubly linked list
+ * @head: head of the stack
  */
-void free_stack(int status, void *arg)
+void free_stack(stack_t *head)
 {
-stack_t **stack;
-stack_t *next;
-(void)status;
-stack = (stack_t **)arg;
-if (*stack)
+stack_t *aux;
+aux = head;
+while (head)
 {
-(*stack)->prev->next = NULL;
-(*stack)->prev = NULL;
+aux = head->next;
+free(head);
+head = aux;
 }
-while (*stack != NULL)
-{
-next = (*stack)->next;
-free(*stack);
-*stack = next;
-}
-var.stack_len = 0;
 }
